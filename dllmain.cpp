52257210cdd69d13297f466dad13486ca912fe87
F0RQU1N <108815933+F0RQU1N@users.noreply.github.com>
2022-07-28 19:32:18 +1000

    Casts

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,7 +10,9 @@ bool __stdcall entry(HINSTANCE dll, unsigned short call_reason, void*)
         
         const uintptr_t start_address = uintptr_t(module_info.lpBaseOfDll);
         const uintptr_t patch_addr = sigscan(start_address, start_address + module_info.SizeOfImage, "7C ? 8B 06 8B CE FF 50 18 50 68 ? ? ? ? FF 15 ? ? ? ? 83 C4 ? B0 ?");
-        patch((void*)patch_addr, (unsigned char*)"\xEB\x21", 2);
+        
+        // patch jl to jmp
+        patch(reinterpret_cast<void*>(patch_addr), reinterpret_cast<unsigned char*>("\xEB\x21"), 2);
     }
     return call_reason;
 }
